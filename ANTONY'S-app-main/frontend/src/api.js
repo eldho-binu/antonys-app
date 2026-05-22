99b4afecf664fbe2e622f2327d92583d76aeb8c0
eldho-binu <eldho.cvv241086@cvv.ac.in>
2026-05-22 11:34:32 +0530

    fixed login api

--- ANTONY'S-app-main/frontend/src/api.js
+++ ANTONY'S-app-main/frontend/src/api.js
@@ -4,3 +4,18 @@ export async function getHello() {
   const res = await fetch(`${API_BASE_URL}/api/hello`);
   return res.json();
 }
+
+export async function loginUser(username, password) {
+  const res = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+
+  return res.json();
+}
